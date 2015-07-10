4cc08f1ea7fc1ddbbb49eef038079e9704a6ea3b
James A Shackleford <shack@linux.com>
2015-07-10 03:04:06 -0400

    Initial commit

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Get dotfiles dir (this script can be ran from anywhere)
+export DOTFILES_DIR EXTRA_DIR
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+EXTRA_DIR="$HOME/.extra"
+
+# Update dotfiles itself via git
+[ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
+
+# Bunch of symlinks
+ln -sfv "$DOTFILES_DIR/tmux/tmux.conf ~/.tmux.conf
