536115fd53d49c82f9e9ddd5088988d2dd61a480
James A Shackleford <shack@linux.com>
2015-08-02 01:13:24 -0400

    zsh: add history.zsh
    Enable zsh history, set length, set location, and move relevant setopts
    into history.zsh

--- zsh/zshrc.d/setopt.zsh
+++ zsh/zshrc.d/setopt.zsh
@@ -15,31 +15,6 @@ setopt pushd_ignore_dups
 setopt extended_glob
 
 
-## History ################################################################
-# allow multiple sessions to append to one zsh history
-setopt append_history
-# save timestamp of command and duration
-setopt extended_history
-# don't add commands to history until session ends
-setopt no_inc_append_history
-# when trimming history, lose oldest dups first
-setopt hist_expire_dups_first
-# do not write events that are dups of prev events
-setopt hist_ignore_dups
-# don't write events if first char is a space
-setopt hist_ignore_space
-# when searching history, don't display dups
-setopt hist_find_no_dups
-# remove extra spaces before writing to history
-setopt hist_reduce_blanks
-# whenever the user enters a line with history substitution, don't execute
-# the line directly; instead, perform history substitution and reload the
-# line into the editing buffer
-setopt hist_verify
-# don't share history between concurrent shell instances
-setopt no_share_history
-
-
 ## Correction #############################################################
 # spelling correction for commands
 setopt correct
