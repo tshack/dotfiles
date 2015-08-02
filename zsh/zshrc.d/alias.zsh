7e70dc6e0559a5c60b9f5d5e8d4234a22d382da5
James A Shackleford <shack@linux.com>
2015-08-02 02:16:12 -0400

    zsh: make less a little smarter/bearable
    * make less not clear screen on start & exit
    * make less auto exit if contents fits on one screen
    * add lless macro for safely long listing dir in color

--- zsh/zshrc.d/alias.zsh
+++ zsh/zshrc.d/alias.zsh
@@ -2,8 +2,8 @@
 if [ -x /usr/bin/dircolors ]; then
     test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
     alias ls='ls --color=auto'
-    alias cls='ls --color=always'
-    alias less='less -R'
+    alias less='less -XF'
+    alias lless='ls -la --color=always | less -R'
 
     alias grep='grep --color=auto'
     alias fgrep='fgrep --color=auto'
