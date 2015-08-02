c03e71a4c1e8dbb8902335723674e13f233f8164
James A Shackleford <shack@linux.com>
2015-08-02 06:43:34 -0400

    zsh: git: end of the git module namespace massacre
    You would think bells would go off if you were developing zsh scripts
    inside a git repo and your zsh tab completion for "git" produces a
    large list of internal git related prompt building functions that nobody
    would ever have the slightest intention of calling from the prompt...

--- zsh/modules/git.zsh
+++ zsh/modules/git.zsh
@@ -7,17 +7,17 @@
 ##################################################
 
 # get the name of the branch we are on
-function git_prompt_info() {
+function _zsh_git_prompt_info() {
   if [[ "$(command git config --get oh-my-zsh.hide-status 2>/dev/null)" != "1" ]]; then
     ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
     ref=$(command git rev-parse --short HEAD 2> /dev/null) || return 0
-    echo "$ZSH_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_GIT_PROMPT_SUFFIX"
+    echo "$ZSH_GIT_PROMPT_PREFIX${ref#refs/heads/}$(_zsh_parse_git_dirty)$ZSH_GIT_PROMPT_SUFFIX"
   fi
 }
 
 
 # Checks if working tree is dirty
-parse_git_dirty() {
+_zsh_parse_git_dirty() {
   local STATUS=''
   local FLAGS
   FLAGS=('--porcelain')
