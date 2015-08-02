4ff439a01520a33d41a5e7a248ecc52f66a20292
James A Shackleford <shack@linux.com>
2015-08-02 01:17:31 -0400

    zsh: bindkeys: add ^J and ^K for fuzzy history

--- zsh/zshrc.d/bindkeys.zsh
+++ zsh/zshrc.d/bindkeys.zsh
@@ -1,2 +1,7 @@
 # use vim mode
 bindkey -v
+
+# start typing + Ctrl+k - fuzzy find history backward
+bindkey "^K" up-line-or-search
+# start typing + Ctrl+j - fuzzy find history forward
+bindkey "^J" down-line-or-search
