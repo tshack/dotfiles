fe39f29a27f3a432ef6b3a360e0d2f16448827a7
James A Shackleford <shack@linux.com>
2015-08-02 01:58:03 -0400

    zsh: bindkeys: make ^left & ^right work as in bash

--- zsh/zshrc.d/bindkeys.zsh
+++ zsh/zshrc.d/bindkeys.zsh
@@ -5,3 +5,8 @@ bindkey -v
 bindkey "^K" up-line-or-search
 # start typing + Ctrl+j - fuzzy find history forward
 bindkey "^J" down-line-or-search
+# despite vim mode, its hard to beat 2 decades of stock bash usage
+## [Ctrl-RightArrow] - move forward one word
+bindkey '^[[1;5C' forward-word     
+## [Ctrl-LeftArrow] - move backward one word
+bindkey '^[[1;5D' backward-word
