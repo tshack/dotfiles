0362dac432c062e382632e60f0b9ee162651e83b
James A Shackleford <shack@linux.com>
2015-08-02 03:25:02 -0400

    zsh: suffix: add file for suffix aliases

---
+++ zsh/zshrc.d/suffix.zsh
@@ -0,0 +1,24 @@
+# file: suffix.zsh
+#
+#   This is where suffix aliases live.
+
+# evince
+alias -s pdf=evince
+alias -s eps=evince
+
+# vlc
+alias -s xspf=vlc
+alias -s mpv=vlc
+alias -s mkv=vlc
+alias -s mp4=vlc
+alias -s mpg=vlc
+alias -s avi=vlc
+
+# less
+alias -s log=less
+
+# eye of gnome
+alias -s jpg=eog
+alias -s png=eog
+alias -s gif=eog
+alias -s tif=eog
