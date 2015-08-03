d8229b2dbd70c75858f44a8bc1be04b6d7927126
James A Shackleford <shack@linux.com>
2015-08-03 10:14:45 -0400

    zsh: modal_prompt: add smkx/rmkx to zle-line-init/-finish

--- zsh/modules/modal_prompt.zsh
+++ zsh/modules/modal_prompt.zsh
@@ -1,4 +1,5 @@
 function zle-line-init zle-keymap-select {
+    echoti smkx
     PROMPT=$ZSH_PROMPT
     case $KEYMAP in
         vicmd)
@@ -16,6 +17,7 @@ zle -N zle-keymap-select
 # this zle widget it executed when return is pressed we are going to use it
 # to "freeze" out the prompt into grayscale or something non-distracting
 function zle-line-finish {
+    echoti rmkx
     PROMPT=$ZSH_PROMPT_FROZEN
     zle reset-prompt
 }
@@ -32,3 +34,5 @@ handle-interrupt() {
 }
 zle -N handle-interrupt
 TRAPINT() { zle && zle handle-interrupt }
+
+export KEYTIMEOUT=1
