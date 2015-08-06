9f080f5e23bac127f7c9320b7e5b4d481cc36fdd
James A Shackleford <shack@linux.com>
2015-08-06 01:31:52 -0400

    zsh: suffix: add aliases for LibreOffice files

--- zsh/zshrc.d/suffix.zsh
+++ zsh/zshrc.d/suffix.zsh
@@ -24,3 +24,24 @@ alias -s jpg=eog
 alias -s png=eog
 alias -s gif=eog
 alias -s tif=eog
+
+# LibreOffice
+alias -s odt=libreoffice
+alias -s ott=libreoffice
+alias -s odm=libreoffice
+alias -s oth=libreoffice
+alias -s ods=libreoffice
+alias -s ots=libreoffice
+alias -s odg=libreoffice
+alias -s otg=libreoffice
+alias -s odp=libreoffice
+alias -s otp=libreoffice
+alias -s odf=libreoffice
+alias -s odb=libreoffice
+#### MS Support
+alias -s doc=libreoffice
+alias -s docx=libreoffice
+alias -s xls=libreoffice
+alias -s xlsx=libreoffice
+alias -s ppt=libreoffice
+alias -s pptx=libreoffice
