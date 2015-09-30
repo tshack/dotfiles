71eb38026d1adca9d20458568242b07e2973eb2f
James A Shackleford <shack@linux.com>
2015-09-29 22:51:38 -0400

    zsh: prompt: add setprompt() function
    Add setprompt function for switching prompt quickly.  Takes one parameter,
    a name of a prompt file from the prompt folder (without the extension).
    
    Example usage:
    
    setprompt basic

--- zsh/zshrc.d/prompt.zsh
+++ zsh/zshrc.d/prompt.zsh
@@ -1,2 +1,13 @@
+ZSH_PROMPT_PATH=${HOME}'/.zsh/prompts'
+
 # Default prompt is the powerline prompt
-source $HOME/.zsh/prompts/powerline.zsh
+source ${ZSH_PROMPT_PATH}/powerline.zsh
+
+setprompt() {
+    local PROMPT_FULLPATH=${ZSH_PROMPT_PATH}/${1}".zsh"
+    if [ -f "${PROMPT_FULLPATH}" ]; then
+        source $PROMPT_FULLPATH
+    else
+        print "'$1' does not exist"
+    fi
+}
