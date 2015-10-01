18420c325e7f3aae0be323883361f5cdb9d216bb
James A Shackleford <shack@linux.com>
2015-09-30 23:42:33 -0400

    zsh: prompt: set prompt based on $TERM
    Now, the default prompt for 256color terminals will be 'powerline' whereas
    for non-256color terminals the default will be 'basic'

--- zsh/zshrc.d/prompt.zsh
+++ zsh/zshrc.d/prompt.zsh
@@ -1,13 +1,33 @@
+# prompt.zsh
+#
+#   This is where we define the location of the
+#   prompts directory, set the default prompt,
+#   and define prompt related functions
+#
+##################################################
+
+# This is where prompt definitions live
 ZSH_PROMPT_PATH=${HOME}'/.zsh/prompts'
 
-# Default prompt is the powerline prompt
-source ${ZSH_PROMPT_PATH}/powerline.zsh
 
+# Utility function for changing the zsh prompt
+# More elegant than manually sourcing font configs
 chprompt() {
     local PROMPT_FULLPATH=${ZSH_PROMPT_PATH}/${1}".zsh"
-    if [ -f "${PROMPT_FULLPATH}" ]; then
+    if [ -f "${PROMPT_FULLPATH}" ]
+    then
         source $PROMPT_FULLPATH
     else
         print "'$1' does not exist"
     fi
 }
+
+
+# If $TERM contains "256color" use the powerline
+# prompt by default, otherwise use the basic prompt
+if strcontains $TERM "256color"
+then
+    chprompt powerline
+else
+    chprompt basic
+fi
