6cb488c33e7216f9689b87af1e18749320adf395
James A Shackleford <shack@linux.com>
2015-09-30 23:18:41 -0400

    zsh: prompt: rename setprompt() to chprompt()

--- zsh/zshrc.d/prompt.zsh
+++ zsh/zshrc.d/prompt.zsh
@@ -3,7 +3,7 @@ ZSH_PROMPT_PATH=${HOME}'/.zsh/prompts'
 # Default prompt is the powerline prompt
 source ${ZSH_PROMPT_PATH}/powerline.zsh
 
-setprompt() {
+chprompt() {
     local PROMPT_FULLPATH=${ZSH_PROMPT_PATH}/${1}".zsh"
     if [ -f "${PROMPT_FULLPATH}" ]; then
         source $PROMPT_FULLPATH
