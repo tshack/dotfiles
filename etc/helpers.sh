86f01bfb94573534f8ab8f743eba548ccdc614e7
James A Shackleford <shack@linux.com>
2015-11-16 10:18:36 -0500

    helpers: fix install_symlink on BSD systems
    GNU's implementation of readlink has some fanciness that is not available
    in the standard POSIX implementation.  So, here is a bunch of extra work.

--- etc/helpers.sh
+++ etc/helpers.sh
@@ -73,10 +73,54 @@ function install_package {
     fi
 }
 
+## POSIX Compliant replacement for readlink ########################
+function _prepend_path_if_relative {
+    case "$2" in
+        /* ) printf '%s\n' "$2" ;;
+         * ) printf '%s\n' "$1/$2" ;;
+    esac
+}
+
+function _canonicalize_dir_path {
+    (cd "$1" 2>/dev/null && pwd -P)
+}
+
+function _canonicalize_file_path {
+    local dir file
+    dir=$(dirname -- "$1")
+    file=$(basename -- "$1")
+    (cd "$dir" 2>/dev/null && printf '%s/%s\n' "$(pwd -P)" "$file")
+}
+
+function resolve_symlinks {
+    local dir_context path
+    path=$(readlink -- "$1")
+    if [ $? -eq 0 ]; then
+        dir_context=$(dirname -- "$1")
+        resolve_symlinks "$(_prepend_path_if_relative "$dir_context" "$path")"
+    else
+        printf '%s\n' "$1"
+    fi
+}
+
+function canonicalize_path {
+    if [ -d "$1" ]; then
+        _canonicalize_dir_path "$1"
+    else
+        _canonicalize_file_path "$1"
+    fi
+}
+
+function realpath {
+    canonicalize_path "$(resolve_symlinks "$1")"
+}
+####################################################################
+
 function install_symlink {
     local SOURCE_FILE=$1
     local TARGET_FILE=$2
-    SOURCE_FILE=$(readlink -e $DOTFILES_DIR/$SOURCE_FILE)
+#    SOURCE_FILE=$(readlink -e $DOTFILES_DIR/$SOURCE_FILE)
+    SOURCE_FILE=$(realpath $DOTFILES_DIR/$SOURCE_FILE)
 
     ln -sfv "$SOURCE_FILE" "$TARGET_FILE"
     if [ $? -ne 0 ]; then
