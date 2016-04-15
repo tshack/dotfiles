1c0d941e954429c50e9c6784d09b7df05c705007
James A Shackleford <shack@linux.com>
2016-04-15 11:35:21 -0400

    zsh: fix backspace issue after exiting NORMAL mode

--- zsh/zshrc.d/bindkeys.zsh
+++ zsh/zshrc.d/bindkeys.zsh
@@ -6,6 +6,12 @@
 
 # despite vim mode, its hard to beat 2 decades of stock bash usage
 ## [Ctrl-RightArrow] - move forward one word
-bindkey '^[[1;5C' forward-word     
+bindkey '^[[1;5C' forward-word
 ## [Ctrl-LeftArrow] - move backward one word
 bindkey '^[[1;5D' backward-word
+
+# zsh's modal behavior is more like vi than vim in some
+# cases (i.e. can't backspace past the last point you
+# entered NORMAL mode).  This fixes that problem, giving
+# more vim-like behavior.
+bindkey "^?" backward-delete-char
