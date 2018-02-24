8192e13958c3e072305dd1e9bef8cbc5218742ba
James A Shackleford <shack@linux.com>
2018-02-24 10:24:39 -0500

    zsh: alias: add '--quoting-style=literal' to ls
    Reverts behavior introduced by coreutils commit 109b922

--- zsh/zshrc.d/alias.zsh
+++ zsh/zshrc.d/alias.zsh
@@ -4,10 +4,15 @@
 #
 ##################################################
 
+# NOTES:
+# 1. coreutils-8.28 changes the default ls quoting behavior
+#    (https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=813164#226)
+#    So, we now use '--quoting-stype=literal' to opt-out
+
 # enable color support of ls and also add handy aliases
 if [ -x /usr/bin/dircolors ]; then
     test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
-    alias ls='ls --color=auto'
+    alias ls='ls --color=auto --quoting-style=literal'
     alias less='less -XF'
     alias lless='ls -la --color=always | less -R'
 
